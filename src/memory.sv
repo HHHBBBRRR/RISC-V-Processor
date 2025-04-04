fe821ff65911ffada6b234708918da09cc8c8e03
HHHBBBRRR <3543977024@qq.com>
2025-04-04 15:20:24 +0800

    ✨ feat: add write mask for memory module
    - Implement wmask input to control which bytes are written
    - Currently supports writing 1, 2, or 4 bytes based on the mask

--- src/memory.sv
+++ src/memory.sv
@@ -6,6 +6,7 @@ module memory (
     input  logic [31:0]  addr1,
     input  logic [31:0]  addr2,
     input  logic [31:0]  wdata,
+    input  logic [ 3:0]  wmask,
     output logic [31:0] rdata1,
     output logic [31:0] rdata2
 );
@@ -26,12 +27,25 @@ module memory (
     /************
     * Write Port
     *************/
-    always_ff @(posedge clk) begin // Synchronous write
-        if (wen) begin
-            RAM[addr2] <= wdata[7:0];
-            RAM[addr2+1] <= wdata[15:8];
-            RAM[addr2+2] <= wdata[23:16];
-            RAM[addr2+3] <= wdata[31:24]; // Write 4 bytes
+    always_ff @(posedge clk) begin
+        if (wen) begin // Synchronous write
+        /* verilator lint_off CASEINCOMPLETE */
+            case (wmask)
+                4'b0001: begin
+                    RAM[addr2] <= wdata[7:0];
+                end 
+                4'b0011: begin
+                    RAM[addr2] <= wdata[7:0];
+                    RAM[addr2+1] <= wdata[15:8];
+                end 
+                4'b1111: begin
+                    RAM[addr2] <= wdata[7:0];
+                    RAM[addr2+1] <= wdata[15:8];
+                    RAM[addr2+2] <= wdata[23:16];
+                    RAM[addr2+3] <= wdata[31:24];
+                end
+            endcase
+        /* verilator lint_on CASEINCOMPLETE */
         end
     end
 
