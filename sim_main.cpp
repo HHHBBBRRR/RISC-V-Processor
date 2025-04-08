b1e125c9aa47394a7899117b0114c6b1b3edaacd
HHHBBBRRR <3543977024@qq.com>
2025-04-08 22:38:52 +0800

    ✨ feat: using DPI-C in memory module
    - Update memory.sv to use DPI-C functions for read and write operations
    - Implement pmem_read and pmem_write functions for accessing memory

--- sim_main.cpp
+++ sim_main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <string_view>
+#include <array>
+#include <stdexcept>
+#include <cstdlib>
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 #include "Vtop.h"
+#include "Vtop__Dpi.h"
+
+#define MBASE 0x8000'0000
+#define MSIZE 0x10'0000 // 1MB
+
+std::array<uint8_t, MSIZE> mem;
+
+std::uint32_t load_img(std::string_view filename);
 
 #define MAX_SIM_TIME 20
 vluint64_t sim_time = 0;
@@ -11,8 +24,17 @@ void reset(Vtop& dut)
     dut.reset = 1;
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <program> [args ...]\n";
+        exit(EXIT_FAILURE);
+    }
+
+    /* init memory and load img*/
+    std::uint32_t img_size = load_img(argv[1]);
+
     Vtop dut;
     Verilated::traceEverOn(true);
     VerilatedVcdC tfp;
@@ -43,4 +65,85 @@ int main(void)
     std::cout << "Simulation finished." << std::endl;
 
     return 0;
+}
+
+std::uint32_t load_img(std::string_view filename)
+{
+    std::ifstream img(filename.data(), std::ios::binary);
+
+    if (!img)
+    {
+        throw std::runtime_error("Failed to open image file");
+    }
+
+    img.seekg(0, std::ios::end);
+    auto size = img.tellg();
+    img.seekg(0, std::ios::beg);
+
+    if (size > MSIZE)
+    {
+        throw std::runtime_error("Image file is too large");
+    }
+
+    if (!img.read(reinterpret_cast<char*>(mem.data()), size))
+    {
+        throw std::runtime_error("Failed to read image file");
+    }
+
+    img.close();
+
+    return size;
+}
+
+int pmem_read(int raddr) 
+{
+    uint32_t addr = static_cast<uint32_t>(raddr - MBASE);
+    uint32_t data{};
+
+    if (addr < 0 || addr >= MSIZE)
+    {
+        std::cout << "Invalid address: " << addr << std::endl;
+        throw std::runtime_error("Invalid address");
+    }
+
+    data |= mem[addr];
+    data |= mem[addr + 1] << 8;
+    data |= mem[addr + 2] << 16;
+    data |= mem[addr + 3] << 24;
+
+    return data;
+}
+
+void pmem_write(int waddr, int wdata, char wmask)
+{
+    uint32_t addr = static_cast<uint32_t>(waddr - MBASE);
+    
+    if (addr < 0 || addr >= MSIZE)
+    {
+        std::cout << "Invalid address: " << addr << std::endl;
+        throw std::runtime_error("Invalid address");
+    }
+
+    if (wmask == 0b0001)   // write 1 byte
+    {   
+        mem[addr] = static_cast<uint8_t>(wdata);
+    }
+    else if (wmask == 0b0011)  // write 2 bytes
+    {
+        
+        mem[addr] = static_cast<uint8_t>(wdata);
+        mem[addr + 1] = static_cast<uint8_t>(wdata >> 8);
+    }
+    else if (wmask == 0b1111)  // write 4 bytes
+    {
+        mem[addr] = static_cast<uint8_t>(wdata);
+        mem[addr + 1] = static_cast<uint8_t>(wdata >> 8);
+        mem[addr + 2] = static_cast<uint8_t>(wdata >> 16);
+        mem[addr + 3] = static_cast<uint8_t>(wdata >> 24); 
+    }
+    else
+    {
+        std::cout << "Invalid write mask: " << wmask << std::endl;
+        throw std::runtime_error("Invalid write mask");
+    }
 }
--- src/memory.sv
+++ src/memory.sv
@@ -1,5 +1,9 @@
 // Two ports RAM
 // port1 is read-only, port2 is read-write.
+
+import "DPI-C" function int pmem_read(input int raddr);
+import "DPI-C" function void pmem_write(input int waddr, input int wdata, input byte wmask);
+
 module memory (
     input  logic           clk,
     input  logic           wen,
@@ -10,42 +14,23 @@ module memory (
     output logic [31:0] rdata1,
     output logic [31:0] rdata2
 );
-    logic [7:0] RAM[1024:0]; // 1KB
-
     /*************
     * Read Ports
     *************/
     // little endian!!!
-    assign rdata1 = { RAM[addr1+3], // Combinational read
-                      RAM[addr1+2],
-                      RAM[addr1+1],
-                      RAM[addr1]  }; // Read 4 bytes
-    assign rdata2 = { RAM[addr2+3],
-                      RAM[addr2+2],
-                      RAM[addr2+1],
-                      RAM[addr2]  };
+    always_comb begin
+        rdata1 = pmem_read(addr1); // Combinational read
+        rdata2 = pmem_read(addr2); // Read 4 bytes
+    end
+
     /************
     * Write Port
     *************/
     always_ff @(posedge clk) begin
         if (wen) begin // Synchronous write
-        /* verilator lint_off CASEINCOMPLETE */
-            case (wmask)
-                4'b0001: begin
-                    RAM[addr2] <= wdata[7:0];
-                end 
-                4'b0011: begin
-                    RAM[addr2] <= wdata[7:0];
-                    RAM[addr2+1] <= wdata[15:8];
-                end 
-                4'b1111: begin
-                    RAM[addr2] <= wdata[7:0];
-                    RAM[addr2+1] <= wdata[15:8];
-                    RAM[addr2+2] <= wdata[23:16];
-                    RAM[addr2+3] <= wdata[31:24];
-                end
-            endcase
-        /* verilator lint_on CASEINCOMPLETE */
+            /* verilator lint_off WIDTHEXPAND */
+            pmem_write(addr2, wdata, wmask);
+            /* verilator lint_on WIDTHEXPAND */
         end
     end
 
