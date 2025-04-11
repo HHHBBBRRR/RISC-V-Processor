9d2a8849a6efc63b76942fd49d9ee73f6ea6bf7a
HHHBBBRRR <3543977024@qq.com>
2025-04-11 15:56:58 +0800

    ✨ feat: implement pipelined architecture for data path module
    - Introduce pipeline registers for each stage: Fetch, Decode, Execute, Memory, and Writeback
    - Refactor data path to support five-stage pipeline

--- src/data_path.sv
+++ src/data_path.sv
@@ -1,178 +1,195 @@
 // Data path module
-module data_path #(
-    parameter PC_START = 32'h8000_0000
-)(
+module data_path (
+    /* external signals */
     input  logic          clk,
     input  logic          reset,
-    /* instruction/data input */
     input  logic  [31:7]  inst,         // data path will not use inst[7:0] because it's opcode
-    input  logic  [31:0]  read_data,     // raw data from memory
+    input  logic  [31:0]  read_data,    // raw data from memory
+    output logic  [31:0]  pc,           // pc used for instruction fetch
+    output logic  [31:0]  address,      // address used for memory read/write
+    output logic  [31:0]  write_data,   // data to be written to memory
     /* control signals */
-    input  logic  [ 2:0]  imm_control,
-    input  logic  [ 2:0]  load_control,
-    input  logic          alu_src_a_sel,
-    input  logic          alu_src_b_sel,
-    input  logic  [ 3:0]  alu_control,
-    input  logic  [ 1:0]  pc_src_sel,
-    input  logic  [ 1:0]  rd_src_sel,
-    input  logic          gpr_wen,
-    /* address/data output */
-    output logic  [31:0]  pc,            // pc used for instruction fetch
-    output logic  [31:0]  address,       // address used for memory read/write
-    output logic  [31:0]  write_data,    // data to be written to memory
+    input  logic  [ 1:0]  F_pc_src_sel,
+    input  logic  [ 2:0]  D_imm_control,
+    input  logic          D_gpr_wen,
+    input  logic          E_alu_src_a_sel,
+    input  logic          E_alu_src_b_sel,
+    input  logic  [ 3:0]  E_alu_control,
+    input  logic  [ 2:0]  M_load_control,
+    input  logic  [ 1:0]  W_rd_src_sel,
     /* branch signals */
-    output logic          negative,
-    output logic          zero,
-    output logic          carry,
-    output logic          overflow
+    output logic          E_negative,
+    output logic          E_zero,
+    output logic          E_carry,
+    output logic          E_overflow
 );
     /* verilator lint_off UNOPTFLAT */
-    // Internal signals
-    logic [31:0] pc_current;
-    logic [31:0] pc_next;
-    logic [31:0] pc_plus_4;
-    logic [31:0] pc_branch_jal_target;
-    logic [31:0] pc_jalr_target;
-    logic [31:0] imm_ext;
-    logic [31:0] rs1;
-    logic [31:0] rs2;
-    logic [31:0] rd;
-    logic [31:0] alu_src_a;
-    logic [31:0] alu_src_b;
-    logic [31:0] alu_result;
-    logic [31:0] load_ext;
+    
+    /* Fetch stage signals */
+    logic [31:7] F_inst;
+    logic [31:0] F_pc_current;
+    logic [31:0] F_pc_plus_4;
+
+    /* Decode stage signals */
+    logic [31:7] D_inst;
+    logic [31:0] D_pc_current;
+    logic [31:0] D_pc_plus_4;
+    logic [31:0] D_rs1;
+    logic [31:0] D_rs2;
+    logic [ 4:0] D_rd_addr;
+    logic [31:0] D_imm_ext;
+
+    /* Execute stage signals */
+    logic [31:0] E_pc_current;
+    logic [31:0] E_pc_plus_4;
+    logic [31:0] E_rs1;
+    logic [31:0] E_rs2;
+    logic [ 4:0] E_rd_addr;
+    logic [31:0] E_imm_ext;
+    logic [31:0] E_write_data;
+    logic [31:0] E_alu_result;
+    logic [31:0] E_pc_jalr_target;
+    logic [31:0] E_pc_auipc_target;
+    logic [31:0] E_pc_branch_jal_target;
+
+    /* Memory stage signals */
+    logic [31:0] M_pc_plus_4;
+    logic [ 4:0] M_rd_addr;
+    logic [31:0] M_alu_result;
+    logic [31:0] M_write_data;
+    logic [31:0] M_pc_auipc_target;
+    logic [31:0] M_load_ext;
+
+    /* Write back stage signals */
+    logic [31:0] W_alu_result;
+    logic [31:0] W_load_ext;
+    logic [31:0] W_pc_auipc_target;
+    logic [31:0] W_pc_plus_4;
+    logic [ 4:0] W_rd_addr;
+    logic [31:0] W_rd;
+
     /* verilator lint_on UNOPTFLAT */
 
-    /*******
-    * PC 
-    ********/
-    flops #(
-        .WIDTH      (32),
-        .SET_VALUE  (PC_START)
-    ) pc_reg (
-        .clk        (clk),
-        .s          (reset),
-        .d          (pc_next),
-        .q          (pc_current)
+    /************
+    * Fetch Unit
+    ************/
+    fetch_unit #(
+        .PC_START 	(32'h8000_0000))
+    FU(
+        .clk                    	(clk                   ),
+        .reset                  	(reset                 ),
+        .pc                     	(pc                    ),
+        .E_pc_branch_jal_target 	(E_pc_branch_jal_target),
+        .E_pc_jalr_target       	(E_pc_jalr_target      ),
+        .F_pc_current           	(F_pc_current          ),
+        .F_pc_plus_4            	(F_pc_plus_4           ),
+        .F_pc_src_sel           	(F_pc_src_sel          )
     );
 
-    export "DPI-C" function get_pc;
-    function void get_pc(output int pc_value);
-        pc_value = pc_current;
-    endfunction
+    assign F_inst = inst;
 
-    mux3 #(
-        .WIDTH      (32)
-    ) pc_mux3 (
-        .d0         (pc_plus_4),
-        .d1         (pc_branch_jal_target),
-        .d2         (pc_jalr_target),
-        .s          (pc_src_sel),
-        .y          (pc_next)
-    );
-
-    /* verilator lint_off PINMISSING */
-    half_adder #(
-        .WIDTH      (32)
-    ) pc_plus_4_adder (
-        .a          (pc_current),
-        .b          (32'h0000_0004),
-        .sum        (pc_plus_4)
-    );
-
-    half_adder #(
-        .WIDTH      (32)
-    ) pc_branch_jal_adder (
-        .a          (pc_current),
-        .b          (imm_ext),
-        .sum        (pc_branch_jal_target)
-    );
-    /* verilator lint_on PINMISSING */
-
-    assign pc = pc_current;
-
-    /********************
-    * Imm extension Unit
-    ********************/
-    imm_extend imm_extension_unit (
-        .inst       (inst[31:7]),
-        .control    (imm_control),
-        .imm_ext    (imm_ext)
-    );
-
-    /*******
-    * GPR
-    ********/
-    register_file GPR (
+    /* Fetch stage pipeline register */
+    flopr #(
+        .WIDTH      (89)
+    ) FU_pipe_reg (
         .clk        (clk),
         .reset      (reset),
-        .wen        (gpr_wen),
-        .raddr1     (inst[19:15]),
-        .raddr2     (inst[24:20]),
-        .waddr      (inst[11:7]),
-        .wdata      (rd),
-        .rdata1     (rs1),
-        .rdata2     (rs2)
-    );
-
-    assign write_data = rs2;
-
-    /*********
-    * ALU
-    *********/ 
-    mux2 #(
-        .WIDTH      (32)
-    ) alu_src_a_mux (
-        .d0         (rs1),
-        .d1         (32'b0), // lui
-        .s          (alu_src_a_sel),
-        .y          (alu_src_a)
-    );
-
-    mux2 #(
-        .WIDTH      (32)
-    ) alu_src_b_mux (
-        .d0         (rs2),
-        .d1         (imm_ext),
-        .s          (alu_src_b_sel),
-        .y          (alu_src_b)
-    );
-
-    alu ALU (
-        .a          (alu_src_a),
-        .b          (alu_src_b),
-        .control    (alu_control),
-        .result     (alu_result),
-        .negative   (negative),
-        .zero       (zero),
-        .carry      (carry),
-        .overflow   (overflow)
-    );
-
-    assign pc_jalr_target = alu_result;
-    assign address = alu_result;
-
-    /**************************
-    * Load data extension unit
-    **************************/
-    load_extend load_extension_unit (
-        .data       (read_data),
-        .funct3     (load_control),
-        .result     (load_ext)
+        .d          ({F_inst, F_pc_current, F_pc_plus_4}),
+        .q          ({D_inst, D_pc_current, D_pc_plus_4})
     );
 
     /*************
-    * Write back
-    **************/
-    mux4 #(
-        .WIDTH      (32)
-    ) rd_mux (
-        .d0         (alu_result),
-        .d1         (load_ext),
-        .d2         (pc_branch_jal_target), // auipc
-        .d3         (pc_plus_4),            // jal/jalr
-        .s          (rd_src_sel),
-        .y          (rd)
+    * Decode Unit
+    *************/
+    decode_unit DU(
+        .clk           	(clk          ),
+        .reset         	(reset        ),
+        .D_inst        	(D_inst       ),
+        .W_rd_addr     	(W_rd_addr    ),
+        .W_rd          	(W_rd         ),
+        .D_rs1         	(D_rs1        ),
+        .D_rs2         	(D_rs2        ),
+        .D_rd_addr     	(D_rd_addr    ),
+        .D_imm_ext     	(D_imm_ext    ),
+        .D_gpr_wen     	(D_gpr_wen    ),
+        .D_imm_control 	(D_imm_control)
+    );
+    
+    /* Decode stage pipeline register */ 
+    flopr #(
+        .WIDTH      (165)
+    ) DU_pipe_reg (
+        .clk        (clk),
+        .reset      (reset),
+        .d          ({D_pc_current, D_pc_plus_4, D_rs1, D_rs2, D_rd_addr, D_imm_ext}),
+        .q          ({E_pc_current, E_pc_plus_4, E_rs1, E_rs2, E_rd_addr, E_imm_ext})
+    );
+
+    /*************
+    * Execute Unit
+    *************/    
+    execute_unit EU(
+        .E_rs1                  	(E_rs1                 ),
+        .E_rs2                  	(E_rs2                 ),
+        .E_pc_current           	(E_pc_current          ),
+        .E_imm_ext              	(E_imm_ext             ),
+        .E_pc_branch_jal_target 	(E_pc_branch_jal_target),
+        .E_pc_jalr_target       	(E_pc_jalr_target      ),
+        .E_pc_auipc_target      	(E_pc_auipc_target     ),
+        .E_alu_result           	(E_alu_result          ),
+        .E_negative             	(E_negative            ),
+        .E_zero                 	(E_zero                ),
+        .E_carry                	(E_carry               ),
+        .E_overflow             	(E_overflow            ),
+        .E_write_data           	(E_write_data          ),
+        .E_alu_src_a_sel        	(E_alu_src_a_sel       ),
+        .E_alu_src_b_sel        	(E_alu_src_b_sel       ),
+        .E_alu_control          	(E_alu_control         )
+    );
+    
+    /* Execute stage pipeline register */
+    flopr #(
+        .WIDTH 	(133)
+    ) EU_pipe_reg(
+        .clk   	(clk),
+        .reset 	(reset),
+        .d     	({E_pc_plus_4, E_rd_addr, E_alu_result, E_write_data, E_pc_auipc_target}),
+        .q     	({M_pc_plus_4, M_rd_addr, M_alu_result, M_write_data, M_pc_auipc_target})
+    );
+    
+    /*************
+    * Memory Unit
+    *************/
+    memory_unit MU(
+        .read_data      	(read_data     ),
+        .address        	(address       ),
+        .write_data     	(write_data    ),
+        .M_alu_result   	(M_alu_result  ),
+        .M_write_data   	(M_write_data  ),
+        .M_load_ext     	(M_load_ext    ),
+        .M_load_control 	(M_load_control)
+    );
+
+    /* Memory stage pipeline register */ 
+    flopr #(
+        .WIDTH 	(133)
+    ) MU_pipe_reg (
+        .clk   	(clk),
+        .reset 	(reset),
+        .d     	({M_alu_result, M_load_ext, M_pc_auipc_target, M_pc_plus_4, M_rd_addr}),
+        .q     	({W_alu_result, W_load_ext, W_pc_auipc_target, W_pc_plus_4, W_rd_addr})
+    );
+    
+    /****************
+    * Writeback Unit
+    ****************/   
+    writeback_unit u_writeback_unit(
+        .W_alu_result      	(W_alu_result     ),
+        .W_load_ext        	(W_load_ext       ),
+        .W_pc_auipc_target 	(W_pc_auipc_target),
+        .W_pc_plus_4       	(W_pc_plus_4      ),
+        .W_rd              	(W_rd             ),
+        .W_rd_src_sel      	(W_rd_src_sel     )
     );
 
 endmodule
